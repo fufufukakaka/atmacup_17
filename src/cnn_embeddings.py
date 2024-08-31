7363d6b4763b36cdc65ef45c74a2ca56f2403bd4
fufufukakaka <demonstrundum@gmail.com>
2024-08-31 01:50:52 +0000

    focal loss を試したけどだめだった

--- src/cnn_embeddings.py
+++ src/cnn_embeddings.py
@@ -14,6 +14,37 @@ import wandb
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class FocalLoss(nn.Module):
+    def __init__(self, alpha=1, gamma=2, reduction="mean"):
+        """
+        alpha (float): バランス係数
+        gamma (float): フォーカルパラメータ (Hard例の学習を抑える)
+        reduction (str): 'none', 'mean', 'sum'のいずれかで、出力をどのように集計するかを指定
+        """
+        super(FocalLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.reduction = reduction
+
+    def forward(self, inputs, targets):
+        # inputs: [batch_size, num_classes]
+        # targets: [batch_size]
+        BCE_loss = F.cross_entropy(inputs, targets, reduction="none")
+        pt = torch.exp(-BCE_loss)  # 予測確率
+        F_loss = self.alpha * (1 - pt) ** self.gamma * BCE_loss
+
+        if self.reduction == "mean":
+            return F_loss.mean()
+        elif self.reduction == "sum":
+            return F_loss.sum()
+        else:
+            return F_loss
+
 
 # Define the CNN model
 class EmbeddingCNN(nn.Module):
