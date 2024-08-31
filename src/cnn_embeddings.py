67d60e5fb8822a05921d641875db1a520422f87b
fufufukakaka <demonstrundum@gmail.com>
2024-08-31 01:44:28 +0000

    うまくいかないmistral

--- src/cnn_embeddings.py
+++ src/cnn_embeddings.py
@@ -73,10 +73,13 @@ def main():
     # Load data
     train_df = pd.read_csv("data/train.csv")
     test_df = pd.read_csv("data/test.csv")
+    embeddings = cloudpickle.load(open("e5_large_embeddings.pkl", "rb"))
 
-    # load npy(SRF mistral) file
-    train_embeddings = np.load("train_embeddings.npy")
-    test_embeddings = np.load("test_embeddings.npy")
+    # Prepare the dataset
+    # train_embeddings = np.load("train_embeddings.npy")
+    # test_embeddings = np.load("test_embeddings.npy")
+    train_embeddings = embeddings["train"]
+    test_embeddings = embeddings["test"]
     train_labels = train_df["Recommended IND"].values
 
     # Initialize StratifiedKFold
